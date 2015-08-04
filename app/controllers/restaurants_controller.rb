49667a4b45da5df075f02c48dd0e5d7358150041
Billy Tandaypan <billy.tandaypan@tektonlabs.com>
2015-08-04 16:31:29 -0500

    gerardo-davila

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,13 @@
 class RestaurantsController < ApplicationController
   def search
+	#client = Foursquare2::Client.new(:api_version => '20120505')
+	#client = Foursquare2::Client.new(:client_id => '2931213', :client_secret => 'asasf')
+  	wordSearch = params[:search]
+  	@result = FOURSQUARE_CLIENT.search_venues(:near => 'Lima', :query => wordSearch)["venues"]
   end
 
   def favorite
+  		@parameter1 = params[:name]  #this is the value to show in favorite page
   end
-end
+
+end 
