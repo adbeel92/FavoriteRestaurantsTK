d8ed4b474a1c50551bce16fcbf4647e7f0c624d5
Billy Tandaypan <billy.tandaypan@tektonlabs.com>
2015-08-07 12:04:45 -0500

    gustavo-galarreta

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,19 @@
 class RestaurantsController < ApplicationController
   def search
+  	city_search=params[:search]
+  	if params.has_key?(:search)
+  		@restaurants=FOURSQUARE_CLIENT.search_venues(:near => city_search, :categoryId =>'4d4b7105d754a06374d81259')["venues"]
+  	else
+  		@restaurants=[]
+  	end
+  end
+  def new_favorite
+  	name_restaurant=params[:name_r]
+  	name_category=params[:category_r]
+  	@current_favorite=Favorite.create({name: name_restaurant , category: name_category})
   end
 
   def favorite
+  	@favorites=Favorite.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'restaurants/search'
 
   get 'restaurants/favorite'
-
+  post 'restaurants/new_favorite'
   root 'restaurants#search'
 
 end
