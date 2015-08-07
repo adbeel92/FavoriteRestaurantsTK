89ecbab80d3bbf6b6b5be315bf5e9308a74ddb73
Billy Tandaypan <billy.tandaypan@tektonlabs.com>
2015-08-07 15:54:24 -0500

    bryam pacco

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,25 @@
 class RestaurantsController < ApplicationController
   def search
+  	if params.has_key?(:search)
+  		@restaurants = FOURSQUARE_CLIENT.search_venues(:near => params[:search], :categoryId => "4d4b7105d754a06374d81259")["venues"]
+  	else
+  		@restaurants = []
+  	end
   end
 
   def favorite
+  	@favorites = Favorite.all
   end
+
+  def create_favorite
+  	if params.has_key?(:name)
+  		name = params[:name]
+  	end
+
+  	if params.has_key?(:category)
+  		category = params[:category]
+  	end
+  	@restaurant= Favorite.create(:name => name, :category => category)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'restaurants/search'
 
   get 'restaurants/favorite'
+  post 'restaurants/create_favorite'
 
   root 'restaurants#search'
 
